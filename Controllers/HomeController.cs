ce70f15ce66da75caec1e6982ae8155389e71b66
obriceno <obriceno@bigobs.cl>
2025-01-31 15:06:11 -0300

    Primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebMVCBigobs.Models;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVCBigobs.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        var message = _configuration["ConnectionStrings"];//["AzureAd:ClientId"];
+        ViewData["Message"] = message;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Conectar a Azure App Configuration
+var appConfigEndpoint = builder.Configuration["AppConfig:Endpoint"];
+/*
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(new Uri(appConfigEndpoint), new DefaultAzureCredential())
+        .Select("ConnectionStrings:*");
+});
+*/
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect("Endpoint=https://llavefinfast.azconfig.io;Id=Q4aD;Secret=9XeCLH2OlmwI1hRQRWcshdJv8KgGdit4I3wyNTYiZtcW5jOPXnJ2JQQJ99BAACYeBjFAwFTjAAACAZACRH4X")
+        .Select(KeyFilter.Any);
+});
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
