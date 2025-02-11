559b879faaafb9df7f26aaee145a9ec9d84248dd
obriceno <obriceno@bigobs.cl>
2025-02-11 15:35:02 -0300

    nueva

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-using Azure.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 
@@ -9,34 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Conectar a Azure App Configuration
 
-
-
-
-
-/*
-builder.Configuration.AddAzureAppConfiguration(options =>
-{
-    options.Connect(new Uri(appConfigEndpoint), new DefaultAzureCredential())
-        .Select("ConnectionStrings:*");
-});
-*/
 var appConfigEndpoint = builder.Configuration["AppConfig:Endpoint"];
-
 builder.Configuration.AddAzureAppConfiguration(options =>
 {
-    options.Connect(new Uri(appConfigEndpoint), new DefaultAzureCredential())
-        .Select("Secret:*");
-});
-
-/*
-var appConfigEndpoint = builder.Configuration["AppConfig:Endpoint"];
- 
-builder.Configuration.AddAzureAppConfiguration(options =>
-{
-    options.Connect("Endpoint=https://llavefinfast.azconfig.io;Id=Q4aD;Secret=9XeCLH2OlmwI1hRQRWcshdJv8KgGdit4I3wyNTYiZtcW5jOPXnJ2JQQJ99BAACYeBjFAwFTjAAACAZACRH4X")
+    options.Connect(appConfigEndpoint)                                                                      //options.Connect("Endpoint=https://llavefinfast.azconfig.io;Id=Q4aD;Secret=9XeCLH2OlmwI1hRQRWcshdJv8KgGdit4I3wyNTYiZtcW5jOPXnJ2JQQJ99BAACYeBjFAwFTjAAACAZACRH4X")
         .Select(KeyFilter.Any);
 });
-*/
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
